6f9b2cfb258a4e68dabf216af0878c4e99e390a6
rodgo1314 <36355523+rodgo1314@users.noreply.github.com>
2021-10-11 05:04:28 -0700

    Initial Commit

---
+++ BucketListLearnPlatform/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BucketListLearnPlatform
+//
+//  Created by Rodrigo Leyva on 10/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
