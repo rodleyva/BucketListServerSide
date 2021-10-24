c3008ec6eb75a0f5a5e12a159cf0c0695b227f10
rodgo1314 <rleyva@codingdojo.com>
2021-10-24 05:10:29 -0700

    commit this

--- BucketListLearnPlatform/AppDelegate.swift
+++ BucketListLearnPlatform/AppDelegate.swift
@@ -17,7 +17,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         return true
     }
-    
 
     // MARK: UISceneSession Lifecycle
 
